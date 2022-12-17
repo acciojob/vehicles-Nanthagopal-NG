dada7a7041a0e6ec94dfcdd75aeb6a40a7eebba4
Nanthagopal <balajinantha68@gmail.com>
2022-12-17 14:52:12 +0530

    Clean Commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,16 +2,8 @@ package com.driver;
 
 public class Boat implements WaterVehicle{
 
-    private String name;
+    public String name;
     private int capacity;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
 
     public void setCapacity(int capacity) {
         this.capacity = capacity;
