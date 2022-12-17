f7da0419e2c69d0f8c0b6c1b91c5571a86c4d8ac
Nanthagopal <balajinantha68@gmail.com>
2022-12-17 14:38:52 +0530

    Methods Specified

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -5,6 +5,22 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
     public  Boat(String name, int capacity){
         this.name = name;
         this.capacity = capacity;
